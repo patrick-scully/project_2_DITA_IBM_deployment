e2d00be6ddf5beb01d5e36b0880d505e09d1baff
Pat Scully <patscully@EPL.IE>
2019-11-14 14:56:35 +0000

    updatedtopic

---
+++ workspace/publications/deployment_and_configuration_guide/out/webhelp-responsive/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,4 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+define(function () {
+return {0:-1,17:-1,15:-1,22:1,18:-1,16:-1,1:-1,21:-1,11:1,19:-1,20:0,14:-1,13:0};
+});
